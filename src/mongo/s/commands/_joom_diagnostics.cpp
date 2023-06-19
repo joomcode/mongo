bb62f1af5580867cdb1e4933247dd231c2d543d6
Vyacheslav Mitrofanov <9631312+unflag@users.noreply.github.com>
2023-06-19 17:49:52 +0100

    Add per-collection extended query statistics to mongos (#2)

---
+++ src/mongo/s/commands/_joom_diagnostics.cpp
@@ -0,0 +1,41 @@
+#include "mongo/platform/basic.h"
+
+#include "mongo/base/status.h"
+#include "mongo/db/commands.h"
+#include "mongo/s/stats/joom_top.h"
+
+namespace mongo {
+namespace {
+
+class JoomDiagnostics : public BasicCommand {
+public:
+    JoomDiagnostics() : BasicCommand("joomdiagnostics", "joomDiagnostics") {};
+
+    AllowedOnSecondary secondaryAllowed(ServiceContext*) const override {
+        return AllowedOnSecondary::kAlways;
+    }
+
+    bool requiresAuth() const override {
+        return false;
+    }
+
+    bool supportsWriteConcern(const BSONObj& cmd) const override {
+        return false;
+    }
+
+    virtual void addRequiredPrivileges(const std::string& dbname,
+                                    const BSONObj& cmdObj,
+                                    std::vector<Privilege>* out) const {}  // No auth required
+
+    bool run(OperationContext* opCtx,
+             const std::string& dbname,
+             const BSONObj& cmdObj,
+             BSONObjBuilder& result) override {
+        JoomTop::get(opCtx->getServiceContext()).append(result);
+        return true;
+    }
+
+} joomDiagnosticsCmd;
+
+}  // namespace
+}  // namespace mongo
---
+++ src/mongo/s/stats/joom_top.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "mongo/db/commands.h"
+#include "mongo/db/operation_context.h"
+#include "mongo/s/stats/joom_latency_histogram.h"
+#include "mongo/util/concurrency/mutex.h"
+#include "mongo/util/string_map.h"
+
+namespace mongo {
+
+class ServiceContext;
+
+/**
+ * tracks usage by collection
+ */
+class JoomTop {
+public:
+    static JoomTop& get(ServiceContext* service);
+
+    JoomTop() = default;
+
+    typedef StringMap<JoomLatencyHistogram> UsageMap;
+
+public:
+    void record(OperationContext* opCtx, bool isError);
+
+    void append(BSONObjBuilder& b);
+
+private:
+    void _appendToUsageMap(BSONObjBuilder& b, const UsageMap& map) const;
+
+    void _incrementHistogram(OperationContext* opCtx,
+                             int64_t latency,
+                             JoomLatencyHistogram* latencyHistogram,
+                             CommandName cmdName,
+                             bool isError);
+
+    mutable SimpleMutex _lock;
+    UsageMap _usage;
+};
+
+inline const CommandName mongoCommandToCommandName(const Command* cmd) {
+    const auto entry = StringToCommandNameMap.find(cmd->getName());
+    if (entry == StringToCommandNameMap.end()) {
+        return CommandName::Other;
+    }
+
+    return entry->second;
+}
+
+}  // namespace mongo
